76db1bc1c51b8152fe2472166abcdf124ca2a227
hyunseo <goatsfish@gmail.com>
2022-12-13 15:06:42 +0900

    init : spring-proxy

---
+++ spring-proxy/src/test/java/com/hyena/springproxy/SpringProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyena.springproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
