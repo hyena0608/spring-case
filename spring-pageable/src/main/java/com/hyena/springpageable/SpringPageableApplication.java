3b48e9edea0f009fbb849ab00daee0cd0c9b5715
hyunseo <goatsfish@gmail.com>
2022-12-31 00:23:15 +0900

    init : spring-pageable

---
+++ spring-pageable/src/main/java/com/hyena/springpageable/SpringPageableApplication.java
@@ -0,0 +1,13 @@
+package com.hyena.springpageable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPageableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPageableApplication.class, args);
+	}
+
+}
---
+++ spring-pageable/src/test/java/com/hyena/springpageable/SpringPageableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyena.springpageable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPageableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
