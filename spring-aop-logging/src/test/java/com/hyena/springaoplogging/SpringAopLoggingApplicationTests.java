2331fa1aa33c00d9e380d5420ae5b6a9c62f8e76
hyunseo <goatsfish@gmail.com>
2023-01-13 11:39:55 +0900

    :tada: init : spring-aop-logging

---
+++ spring-aop-logging/src/test/java/com/hyena/springaoplogging/SpringAopLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyena.springaoplogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
