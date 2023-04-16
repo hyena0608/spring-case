6a9d97a8a219a46eb3f2737bcb29e7f0e50ce740
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-16 12:40:54 +0900

    init : spring-argument-resolver

---
+++ spring-argument-resolver/src/main/java/com/example/springargumentresolver/SpringArgumentResolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.springargumentresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArgumentResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringArgumentResolverApplication.class, args);
+    }
+
+}
